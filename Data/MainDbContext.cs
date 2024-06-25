615f0e64cc42f73f606fbc3d9c4cdab5cb2700e0
mbenfedane <mbenfedane@NEOS-NBK915>
2024-06-25 14:41:52 +0200

    Initial commit

---
+++ Data/MainDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Weather.Models;
+
+namespace Weather.Data
+{
+    public class MainDbContext : DbContext
+    {
+        public MainDbContext(DbContextOptions<MainDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<City> cities { get; set; }
+    }
+}
---
+++ Models/City.cs
@@ -0,0 +1,10 @@
+﻿namespace Weather.Models
+{
+    public class City
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+    }
+}
